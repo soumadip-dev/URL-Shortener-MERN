799f691f87c0755459cd5423049e1dae3640fc8f
soumadip-dev <soumadipmajila@gmail.com>
2025-07-10 11:00:00 +0530

    refactor: update create short URL mutation to handle custom slug

--- frontend/src/api/urlService.api.js
+++ frontend/src/api/urlService.api.js
@@ -1,8 +1,10 @@
 import axiosInstance from '../utils/axiosInstance.js';
 
-export const getShortUrl = async url => {
+//* Mutation function to Create a short URL
+export const getShortUrl = async ({ url, slug = null }) => {
   const response = await axiosInstance.post('/shorturl/create', {
-    url: url,
+    url,
+    slug,
   });
   return response.data;
 };
