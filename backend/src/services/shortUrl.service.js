163234bc927e87ca0963b4146be266b1ae3be0e8
soumadip-dev <soumadipmajila@gmail.com>
2025-07-16 12:00:00 +0530

    refactor: update service to use new DAO function for short URL creation when user is present

--- backend/src/services/shortUrl.service.js
+++ backend/src/services/shortUrl.service.js
@@ -1,5 +1,10 @@
 import { generateNanoid } from '../utils/helper.js';
-import { saveShortUrl, findUrlByShortId, getCustomeShortUrl } from '../dao/shortUrl.dao.js';
+import {
+  saveShortUrl,
+  saveShortUrlUser,
+  findUrlByShortId,
+  getCustomeShortUrl,
+} from '../dao/shortUrl.dao.js';
 
 //* Function to Creates a short URL for an anonymous (non-authenticated) user.
 export const createAnonymousShortUrl = async url => {
@@ -14,7 +19,7 @@ export const createUserShortUrl = async (url, userId, slug = null) => {
   const exiists = await getCustomeShortUrl(slug);
   if (exiists) throw new Error('This custom short url already exists');
 
-  await saveShortUrl({ full_url: url, short_url: shortUrl, user: userId });
+  await saveShortUrlUser({ full_url: url, short_url: shortUrl, user: userId });
   return shortUrl; // Return the short URL
 };
 
