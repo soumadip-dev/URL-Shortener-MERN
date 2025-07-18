77eb55a31af1bc2bbcac53474fb20e90c14b0d68
soumadip-dev <soumadipmajila@gmail.com>
2025-07-18 12:00:00 +0530

    chore: correct comments in shortUrl.service.js for clarity

--- backend/src/services/shortUrl.service.js
+++ backend/src/services/shortUrl.service.js
@@ -1,21 +1,21 @@
 import { generateNanoid } from '../utils/helper.js';
 import { saveShortUrl, findUrlByShortId } from '../dao/shortUrl.dao.js';
 
-// Function to Creates a short URL for an anonymous (non-authenticated) user.
+//* Function to Creates a short URL for an anonymous (non-authenticated) user.
 export const createAnonymousShortUrl = async url => {
   const shortUrl = generateNanoid(7); // Generate a unique short URL using Nanoid
   await saveShortUrl({ full_url: url, short_url: shortUrl });
   return shortUrl; // Return the generated short URL
 };
 
-// Function to Creates a short URL for an authenticated user (logic can be extended later)
-export const createUserShortUrl = async (url, userId) => {
+//* Function to Creates a short URL for an authenticated user (logic can be extended later)
+export const createUserShortUrl = async (url, userId, slug = null) => {
   const shortUrl = generateNanoid(7); // Generate a unique short URL using Nanoid
   await saveShortUrl({ full_url: url, short_url: shortUrl, user: userId });
   return shortUrl; // Return the generated short URL
 };
 
-// Function to retrieve the full URL using the short URL ID
+//* Function to retrieve the full URL using the short URL ID
 export const getFullUrl = async shortUrlId => {
   return await findUrlByShortId(shortUrlId); // Find and return the document with the matching short URL
 };
