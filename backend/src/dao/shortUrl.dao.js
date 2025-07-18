f7d8f43b4be6241defb7eede29aa5419c665cc36
soumadip-dev <soumadipmajila@gmail.com>
2025-07-18 12:00:00 +0530

    feat: add DAO function to find document by short URL

--- backend/src/dao/shortUrl.dao.js
+++ backend/src/dao/shortUrl.dao.js
@@ -1,5 +1,6 @@
 import urlSchema from '../models/shorturl.model.js';
 
+//* Saves a new short URL to the database
 export const saveShortUrl = async ({ full_url, short_url }) => {
   const newUrl = new urlSchema({
     full_url,
@@ -8,6 +9,12 @@ export const saveShortUrl = async ({ full_url, short_url }) => {
   await newUrl.save();
 };
 
+//* Finds a document in the database with the matching short URL
 export const findUrlByShortId = async short_url => {
-  return await urlSchema.findOne({ short_url });
+  return await urlSchema.findOneAndUpdate({ short_url }, { $inc: { clicks: 1 } });
+};
+
+//* Finds a document in the database with the matching short URL
+export const getCustomeShortUrl = async slug => {
+  return await urlSchema.findOne({ short_url: slug });
 };
