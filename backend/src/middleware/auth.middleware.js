0c80e02a19f21b329d685cc97e282be84cd23d27
soumadip-dev <soumadipmajila@gmail.com>
2025-07-25 12:00:00 +0530

    feat: write middleware to verify if user is logged in

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { ENV } from '../config/env.js';
+
+export const isLoogedIn = async (req, res, next) => {
+  try {
+    // Get the token from the cookies
+    let token = req.cookies?.jwt;
+
+    // If no token is present, return an unauthorized response
+    if (!token) {
+      console.log('No token found');
+      return res.status(401).json({ message: 'Unauthorized', success: false });
+    }
+
+    // Verify the token using the secret key
+    const decoded = jwt.verify(token, ENV.JWT_SECRET);
+
+    // Add the decoded user to the request object for later use
+    req.user = decoded;
+    next();
+  } catch (error) {
+    // Handle any errors that occur during the middleware execution
+    console.error('Middleware error:', error.message);
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
