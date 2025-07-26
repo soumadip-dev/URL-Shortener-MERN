c9cd24d60c8bf629146ecf749e9f05c6bbfa4774
soumadip-dev <soumadipmajila@gmail.com>
2025-07-26 12:00:00 +0530

    chore: add authentication-related environment variables to env.js utility

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -7,4 +7,10 @@ export const ENV = {
   NODE_ENV: process.env.NODE_ENV,
   MONGODB_URI: process.env.MONGODB_URI,
   APP_URL: process.env.APP_URL,
+  MAILTRAP_HOST: process.env.MAILTRAP_HOST,
+  MAILTRAP_PORT: process.env.MAILTRAP_PORT,
+  MAILTRAP_USERNAME: process.env.MAILTRAP_USERNAME,
+  MAILTRAP_PASSWORD: process.env.MAILTRAP_PASSWORD,
+  MAILTRAP_SENDEREMAIL: process.env.MAILTRAP_SENDEREMAIL,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
