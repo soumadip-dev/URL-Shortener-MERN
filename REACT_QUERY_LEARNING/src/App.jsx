975e3ccf4be59a60dd3067a1de49f0837b637e02
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    feat: set up basic data fetching using fetch and React Query

--- REACT_QUERY_LEARNING/src/App.jsx
+++ REACT_QUERY_LEARNING/src/App.jsx
@@ -1,4 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchPosts } from './api/api.js';
+
 const App = () => {
+  const { data, isLoading, isError, status } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+  console.log('Data:', data, 'Loading:', isLoading, 'Error: ', isError, 'Status: ', status);
+
   return <div>App</div>;
 };
 export default App;
