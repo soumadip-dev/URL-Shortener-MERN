4cf30d08be9225c5bc2718e34f536be74c1a0099
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    chore: add ReactQueryDevtools for debugging React Query state

--- REACT_QUERY_LEARNING/src/App.jsx
+++ REACT_QUERY_LEARNING/src/App.jsx
@@ -1,14 +1,6 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from './api/api.js';
 import Post_list from './components/Post_list.jsx';
 
 const App = () => {
-  const { data, isLoading, isError, status } = useQuery({
-    queryKey: ['posts'],
-    queryFn: fetchPosts,
-  });
-  console.log('Data:', data, 'Loading:', isLoading, 'Error: ', isError, 'Status: ', status);
-
   return (
     <div>
       <h1>Soumadip Majila</h1>
