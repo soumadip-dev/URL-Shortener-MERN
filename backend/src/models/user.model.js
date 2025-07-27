57e8e38fb055fdd01b3a8f5b5f3f333d96b61c85
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    feat: create userAuth model with pre-save hook to hash password using bcrypt

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: false,
+      default:
+        'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png',
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  this.email = this.email.toLowerCase();
+  next();
+});
+
+// Create the user model
+const User = mongoose.model('User', userSchema);
+
+// Export the user model
+export default User;
