53a90cd46d596dedd04104b375f91f0bc9e6a0c8
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    feat: add functions to fetch tags and create new post

--- REACT_QUERY_LEARNING/src/api/api.js
+++ REACT_QUERY_LEARNING/src/api/api.js
@@ -4,5 +4,20 @@ const fetchPosts = async () => {
   const postData = await response.json();
   return postData;
 };
+const fetchTags = async () => {
+  const response = await fetch('http://localhost:3000/tags');
+  const tagsData = await response.json();
+  return tagsData;
+};
+const addPost = async post => {
+  const response = await fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(post),
+  });
+  return response.json();
+};
 
-export { fetchPosts };
+export { fetchPosts, addPost, fetchTags };
