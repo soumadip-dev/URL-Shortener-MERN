80519693440d5ada76ff979ddc3128c7973084dc
soumadip-dev <soumadipmajila@gmail.com>
2025-07-27 12:00:00 +0530

    feat: add form component to create new posts

--- REACT_QUERY_LEARNING/src/components/Post_list.jsx
+++ REACT_QUERY_LEARNING/src/components/Post_list.jsx
@@ -1,9 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from '../api/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { addPost, fetchPosts, fetchTags } from '../api/api';
 
-const Post_list = () => {
+const PostList = () => {
   const {
-    data: posstData,
+    data: postData,
     isError,
     isLoading,
     error,
@@ -12,12 +12,53 @@ const Post_list = () => {
     queryFn: fetchPosts,
   });
 
+  const {
+    data: tagsData,
+    isError: isTagError,
+    isLoading: isTagLoading,
+    error: tagError,
+  } = useQuery({
+    queryKey: ['tags'],
+    queryFn: fetchTags,
+  });
+
+  const {
+    mutate,
+    isError: isPostError,
+    isPending,
+    error: postError,
+    reset,
+  } = useMutation({
+    mutationFn: addPost,
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const title = formData.get('title');
+    const tags = Array.from(formData.keys()).filter(key => formData.get(key) === 'on');
+    console.log(title, tags);
+  };
+
   return (
     <div className="post-container">
+      <form className="post-form">
+        <input type="text" placeholder="Enter your post..." className="postbox" name="title" />
+
+        <div className="tags">
+          {tagsData?.map(tag => (
+            <div className="tag-option" key={tag}>
+              <input type="checkbox" id={tag} name={tag} />
+              <label htmlFor={tag}>{tag}</label>
+            </div>
+          ))}
+        </div>
+      </form>
+
       {isLoading && <p className="loading">Loading...</p>}
       {isError && <p className="error">{error?.message}</p>}
 
-      {posstData?.map(post => (
+      {postData?.map(post => (
         <div className="post-card" key={post.id}>
           <h2 className="post-title">{post.title}</h2>
           <div className="tag-container">
@@ -33,4 +74,4 @@ const Post_list = () => {
   );
 };
 
-export default Post_list;
+export default PostList;
