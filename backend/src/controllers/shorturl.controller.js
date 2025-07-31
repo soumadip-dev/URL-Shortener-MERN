1a1c5e9359228175bad252a3d40d710a6b954333
soumadip-dev <soumadipmajila@gmail.com>
2025-07-31 23:18:56 +0530

    feat(controller): add logic to generate and store short URLs with nanoid

--- backend/src/controllers/shorturl.controller.js
+++ backend/src/controllers/shorturl.controller.js
@@ -1,9 +1,15 @@
 import { nanoid } from 'nanoid';
+import urlSchema from '../models/shorturl.model.js';
 
 const shortUrlController = async (req, res) => {
   const { url } = req.body;
-  console.log(url);
-  res.send(nanoid(7));
+  const shortUrl = nanoid(7);
+  const newUrl = new urlSchema({
+    full_url: url,
+    short_url: shortUrl,
+  });
+  newUrl.save();
+  res.status(201).json({ shortUrl });
 };
 
 export { shortUrlController };
