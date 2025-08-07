9d17c0fa4f46bf1e13d06d264f5ba448762fc444
soumadip-dev <soumadipmajila@gmail.com>
2025-08-07 13:14:25 +0530

    refactor(auth): clean up auth DAO by removing unused methods
    Only keep findUserByEmail and createUser as they are the only ones used in the service.

--- backend/src/dao/auth.dao.js
+++ backend/src/dao/auth.dao.js
@@ -7,61 +7,9 @@ const authDAO = {
     return await User.findOne({ email });
   },
 
-  //* Find user by ID
-  findUserById: async userId => {
-    return await User.findById(userId).select('-password');
-  },
-
   //* Create new user
   createUser: async userData => {
     return await User.create(userData);
-  },
-
-  //* Find user by verification token
-  findUserByVerificationToken: async token => {
-    return await User.findOne({ verificationToken: token });
-  },
-
-  //* Update user verification status
-  updateUserVerification: async (userId, isVerified) => {
-    return await User.findByIdAndUpdate(
-      userId,
-      { isVerified, verificationToken: undefined },
-      { new: true }
-    );
-  },
-
-  //* Find user by reset password token
-  findUserByResetToken: async token => {
-    return await User.findOne({
-      resetPasswordToken: token,
-      resetPasswordExpiry: { $gt: Date.now() },
-    });
-  },
-
-  //* Update user password
-  updateUserPassword: async (userId, newPassword) => {
-    return await User.findByIdAndUpdate(
-      userId,
-      {
-        password: newPassword,
-        resetPasswordToken: undefined,
-        resetPasswordExpiry: undefined,
-      },
-      { new: true }
-    );
-  },
-
-  //* Set reset password token
-  setResetPasswordToken: async (userId, token) => {
-    return await User.findByIdAndUpdate(
-      userId,
-      {
-        resetPasswordToken: token,
-        resetPasswordExpiry: Date.now() + 10 * 60 * 1000, // 10 minutes
-      },
-      { new: true }
-    );
   },
 };
 
