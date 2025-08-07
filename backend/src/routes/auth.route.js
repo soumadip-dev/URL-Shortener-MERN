599c2c7ed24d0734fdfdf93a49f9b1b7eb3a9842
soumadip-dev <soumadipmajila@gmail.com>
2025-08-07 13:08:34 +0530

    chore: remove unused routes from auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,25 +1,13 @@
 import { Router } from 'express';
-import {
-  registerUser,
-  verifyUser,
-  loginUser,
-  getMe,
-  logout,
-  forgotPassword,
-  resetPassword,
-} from '../controllers/auth.controller.js';
+import { registerUser, loginUser, logout } from '../controllers/auth.controller.js';
 import { isLoogedIn } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 // Define Rouites
 router.post('/register', registerUser);
-router.get('/verify/:token', verifyUser);
 router.post('/login', loginUser);
-router.get('/me', isLoogedIn, getMe);
 router.get('/logout', logout);
-router.post('/forgot-password', forgotPassword);
-router.put('/reset-password/:token', resetPassword);
 
 // Export the router
 export default router;
