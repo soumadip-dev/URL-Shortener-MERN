34c3d1ea24c66aac4c1aee33f3a40d437f611ed8
soumadip-dev <soumadipmajila@gmail.com>
2025-08-07 23:04:21 +0530

    chore: set up main.jsx with TanStack Router configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,22 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+// Import the generated route tree
+import { routeTree } from './routing/routeTree.js';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <RouterProvider router={router} />
+    <TanStackRouterDevtools router={router} />
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
