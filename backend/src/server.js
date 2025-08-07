0f568845cd61e59d653064ae9df443cd767ce8e4
soumadip-dev <soumadipmajila@gmail.com>
2025-08-07 12:42:35 +0530

    chore: correct comments in server.js for clarity

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-// Import necessary modules
+//* Import necessary modules
 import express from 'express';
 import cors from 'cors';
 import { ENV } from './config/env.js';
@@ -8,22 +8,22 @@ import auth_routes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import { attachUser } from './utils/attachUser.js';
 
-// Initialize an Express application
+//* Initialize an Express application
 const app = express();
 
-// Set the port to listen on
+//* Set the port to listen on
 const PORT = ENV.PORT || 8080;
 
-// Middleware to parse JSON and URL-encoded data
+//* Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to parse cookies
+//* Middleware to parse cookies
 app.use(cookieParser());
 
 app.use(attachUser);
 
-// Enable CORS
+//* Enable CORS
 app.use(
   cors({
     origin: 'http://localhost:5173', // or your frontend URL
@@ -31,16 +31,16 @@ app.use(
   })
 );
 
-// Handle GET requests to the root URL
+//* Handle GET requests to the root URL
 app.get('/', (req, res) => {
   res.send('<h1>Hello From URL Shortener Backend!</h1>');
 });
 
-// Use the short URL routes
+//* Use the short URL routes
 app.use('/shorturl', shortUrlRoutes);
 app.use('/auth', auth_routes);
 
-// Function to connect to DB and start the server
+//* Function to connect to DB and start the server
 const startServer = async () => {
   try {
     await connectDB(); // Ensure DB is connected before starting the server
