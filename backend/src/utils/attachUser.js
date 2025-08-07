f6dec1379df71f9dff0fa129d1aa98617a79f83f
soumadip-dev <soumadipmajila@gmail.com>
2025-08-07 13:01:12 +0530

    chore: remove unwanted log from attachUser.js

--- backend/src/utils/attachUser.js
+++ backend/src/utils/attachUser.js
@@ -6,7 +6,6 @@ export const attachUser = async (req, res, next) => {
   try {
     const decoded = jwt.verify(token, ENV.JWT_SECRET);
     req.user = decoded;
-    console.log('Akhane aschi');
   } catch (error) {
     console.error('Error attaching user:', error.message);
     return res.status(401).json({ message: 'Unauthorized', success: false });
