bddb5211fe8eb835dc1d8a3ffa663ac5bbb80ce5
soumadip-dev <soumadipmajila@gmail.com>
2025-08-09 20:13:57 +0530

    feat: create controller to fetch all URLs created by the authenticated user

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import urlSchema from '../models/shorturl.model.js';
+
+//* Coontroller for getting all Url of the user
+const getAllUserUrls = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const userUrls = await urlSchema.find({ user: id });
+    res.status(200).json({ userUrls, success: true });
+  } catch (error) {
+    console.error(error.message);
+    const statusCode = error.message.includes('not found') ? 404 : 400;
+    res.status(statusCode).json({ message: error.message, success: false });
+  }
+};
+
+//* Export the controllers
+export { getAllUserUrls };
