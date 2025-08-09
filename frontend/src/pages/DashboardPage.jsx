e38937ad93bb2295b3bdf7b5f049a965e4489489
soumadip-dev <soumadipmajila@gmail.com>
2025-08-09 13:56:23 +0530

    style: enhance UI of DashboardPage component

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,17 @@
 import UrlShortner from '../components/UrlForm';
+import UserUrl from '../components/UserUrl';
+
 const DashboardPage = () => {
-  return <UrlShortner />;
+  return (
+    <div className="flex flex-row justify-center ">
+      <div className="flex-1">
+        <UrlShortner />
+      </div>
+      <div className="flex-2">
+        <UserUrl />
+      </div>
+    </div>
+  );
 };
+
 export default DashboardPage;
